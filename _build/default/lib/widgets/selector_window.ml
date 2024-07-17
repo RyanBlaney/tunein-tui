e4ef74be856caccd739b1d7cd720b3cbc8e8cd43
RyanBlaney <ryanblaney2004@gmail.com>
2024-07-16 22:39:08 -0700

    Fixed the info panel dimensions

--- _build/default/lib/widgets/selector_window.ml
+++ _build/default/lib/widgets/selector_window.ml
@@ -4,9 +4,9 @@ open LTerm_geom
 let draw_selector_window ctx size = 
   let padding = size.rows / 32 in
   let rect = { 
-    row1 = (size.rows / 3 + padding); 
+    row1 = (size.rows / 3 + padding * 4); 
     col1 = (size.cols / 3 + padding * 8); 
-    row2 = (size.rows / 3 + padding * 4); 
+    row2 = (size.cols / 2); 
     col2 = size.cols - padding } in
   let label = Zed_string.of_utf8 "Info" in
   let style = { LTerm_style.none with bold = Some true; underline = Some false } in
--- lib/widgets/my_library.ml
+++ lib/widgets/my_library.ml
@@ -3,7 +3,11 @@ open LTerm_geom
 
 let draw_library ctx size = 
   let padding = size.rows / 32 in
-  let rect = { row1 = (size.rows / 3 + padding); col1 = padding; row2 = (size.cols / 2); col2 = (size.cols / 3 + (7 * padding)) } in
+  let rect = { 
+    row1 = (size.rows / 3 + padding); 
+    col1 = padding; 
+    row2 = (size.cols / 2); 
+    col2 = (size.cols / 3 + (7 * padding)) } in
   let label = Zed_string.of_utf8 "My Library" in
   let style = { LTerm_style.none with bold = Some true; underline = Some false } in
   draw_frame_labelled ctx rect ~style ~alignment:H_align_center label LTerm_draw.Heavy;
